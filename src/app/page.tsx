c83e12668133d44e6ccc66dc455b9aa7192f4db4
Nirvana55 <lamabruno55@gmail.com>
2024-01-21 18:17:40 +0545

    add slider and carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Carousel from "../../components/carousel/Carousel";
 import Navbar from "../../components/navbar/Navbar";
+import { CommonContainerStyle } from "../../styles/layoutStyles";
 
 export default function Home() {
   return (
-    <main>
-      <Navbar />
-    </main>
+    <>
+      <CommonContainerStyle>
+        <Navbar />
+      </CommonContainerStyle>
+      <Carousel />
+    </>
   );
 }
--- styles/layoutStyles.tsx
+++ styles/layoutStyles.tsx
@@ -4,12 +4,13 @@ import { styled } from "@mui/material/styles";
 import { Box } from "@mui/material";
 
 export const LayoutContainerStyle = styled(Box)(({ theme }) => ({
-  width: "80rem",
-  margin: "0 auto",
   minHeight: "100vh",
   height: "auto",
-  display: "flex",
-  flexDirection: "column",
+}));
+
+export const CommonContainerStyle = styled(Box)(({ theme }) => ({
+  width: "85rem",
+  margin: "0 auto",
   [theme.breakpoints.down("lg")]: {
     width: "60rem",
   },
