563edeb985e9df0e88c8511ec0c5fe38938de794
Nirvana55 <lamabruno55@gmail.com>
2024-01-23 08:01:20 +0545

    refactor: remove mui and add tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import Carousel from "../../components/carousel/Carousel";
 import Navbar from "../../components/navbar/Navbar";
-import { CommonContainerStyle } from "../../styles/layoutStyles";
 
 export default function Home() {
   return (
     <>
-      <CommonContainerStyle>
-        <Navbar />
-      </CommonContainerStyle>
+      <Navbar />
       <Carousel />
     </>
   );
