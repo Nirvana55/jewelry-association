9a97dc795e62ddf440c6764821b8b2d7043cf0b9
Nirvana55 <lamabruno55@gmail.com>
2024-01-24 22:33:12 +0545

    refactor: update navbar styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,39 @@ module.exports = {
     "./public/**/*.html",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      blue: "#1fb6ff",
+      purple: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+      primary: "#305586",
+      "primary-background": "#eef3f9",
+      "primary-danger": "#BD2729",
+      "primary-link": "#2F6BAE",
+    },
+    fontSize: {
+      customSize: "16px",
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
