8add9115a4ef9aebbcf123a93cfedf0d96ac21cf
Nirvana55 <lamabruno55@gmail.com>
2024-02-17 22:07:23 +0545

    feat: update carousel responsive

--- components/carousel/Carousel.tsx
+++ components/carousel/Carousel.tsx
@@ -6,7 +6,12 @@ import { useTransition, animated } from "@react-spring/web";
 import "./styles.css";
 import { useState } from "react";
 import { BsArrowUpCircle, BsArrowDownCircle } from "react-icons/bs";
-import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
+import {
+  IoIosArrowUp,
+  IoIosArrowDown,
+  IoIosArrowDropleft,
+  IoIosArrowDropright,
+} from "react-icons/io";
 
 const imageSlides = ["/1.jpeg", "/2.jpeg", "/3.jpeg"];
 
@@ -60,7 +65,7 @@ const CarouselBar = () => {
         key={index}
         className={`indicator ${
           index === carouselIndex ? "borderBottomIndicatorActive" : ""
-        } text-white py-5 cursor-pointer borderBottomIndicator`}
+        } text-white lg:py-5 cursor-pointer borderBottomIndicator mx-5 py-1`}
         onClick={() => handleSlideChange(index)}
       >
         0{index + 1}
@@ -84,33 +89,36 @@ const CarouselBar = () => {
           />
         ))}
       </div>
-      <div className='bg-test bg-opacity-45 w-100% h-[800px] top-0 bottom-0 left-0 right-0 absolute z-10'></div>
+      <div className='bg-test bg-opacity-60 w-100% h-[800px] top-0 bottom-0 left-0 right-0 absolute z-10'></div>
       <div
         onClick={handlePrevious}
-        className='z-20 absolute left-7 top-[35%] cursor-pointer'
+        className='z-20 absolute left-[20%] right-[50%] bottom-10 lg:left-7 lg:top-[35%] cursor-pointer'
       >
-        <IoIosArrowUp className='text-white arrowFontSize border-spacing-2' />
+        <IoIosArrowUp className='hidden lg:block text-white arrowFontSize border-spacing-2' />
+        <IoIosArrowDropleft className='block lg:hidden text-white arrowFontSize border-spacing-2' />
       </div>
       <div
         onClick={handleNext}
-        className='z-20 absolute border-solid left-7 top-[70%] cursor-pointer'
+        className='z-20 absolute right-[20%] bottom-10 border-solid lg:left-7 lg:top-[70%] cursor-pointer'
       >
-        <IoIosArrowDown className='text-white arrowFontSize' />
+        <IoIosArrowDown className='hidden lg:block text-white arrowFontSize' />
+        <IoIosArrowDropright className='block lg:hidden text-white arrowFontSize border-spacing-2' />
       </div>
-      <div className='absolute top-[40%] left-8 text-center z-20 w-[15px]'>
+      <div className='absolute bottom-10 left-1/2 transform -translate-x-1/2 lg:top-[40%] lg:left-10 text-center z-20 flex flex-row lg:flex-col'>
         {renderIndicators()}
       </div>
       <div className='container z-20 mx-auto'>
-        <div className='absolute bottom-20 max-lg:left-16 max-xl:left-16  z-20 rounded'>
-          <div className=' py-3 px-5 lg:w-[70%] xl:w-[100%]'>
-            <p className='text-white font-bold welcomeHeaderText text-xl sm:text-2xl md:text-3xl lg:titleHeader xl:titleHeader'>
-              Welcome To Negja
-            </p>
-            <p className='text-white w-full md:w-[90%] lg:w-[80%] xl:w-[100%]'>
-              Nepal&apos;s Jewelry Association
-            </p>
-            <Button className='bg-primary-danger mt-5'>Explore Now</Button>
-          </div>
+        <div className='absolute top-[50%] max-[412px]:left-2 max-lg:left-16 max-xl:left-16 z-20 rounded'>
+          <p className='text-white font-bold text-[40px] md:text-[50px]'>
+            Welcome To Negja
+          </p>
+          <p className='text-white w-full sm:w-[90%] md:w-[85%] lg:w-[80%] xl:w-[75%] text-lg sm:text-xl md:text-2xl'>
+            Track the latest gold and silver rates with our dynamic graphs and
+            make informed decisions.
+          </p>
+          <Button className='bg-primary-danger mt-5 text-white font-medium py-2 px-4 rounded hover:bg-primary-dark transition duration-300'>
+            Explore Now
+          </Button>
         </div>
       </div>
     </div>
